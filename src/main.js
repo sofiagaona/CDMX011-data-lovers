c9520e4fd7791acc2de99f6189239bf7ed218e53
anasofiamoreno <84933610+anasofiamoreno@users.noreply.github.com>
2021-07-08 12:23:14 -0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,29 @@
 import { example } from './data.js';
-// import data from './data/lol/lol.js';
 import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
 
 console.log(example, data);
+
+let objFiltoUno = document.getElementById("filtrouno");
+let objTexBox1 = document.getElementById("txbxnumeropokemon");
+document.getElementById("btnbuscar").addEventListener("click",filtrar);
+document.getElementById("filtrouno").addEventListener("toggle",fnOcultarTxBx)
+
+document.getElementById("test").innerHTML = data.pokemon[0]['size']['height'];
+
+
+function filtrar(){
+  console.log(objTexBox1.value);
+  document.getElementById("test").innerHTML = data['pokemon'][objTexBox1.value-1]['num'];
+  document.getElementById("test").innerHTML = data['pokemon'][objTexBox1.value-1]['name'];
+}
+
+function fnOcultarTxBx(){
+
+  console.log(objFiltoUno.value);
+  if(objFiltoUno.value==1){
+
+  }
+}
+
+
+ 
