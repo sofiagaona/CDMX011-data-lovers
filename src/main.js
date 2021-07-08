1adb5e4075d4ac95aebad0a4e4ec25a165ae0cb0
anasofiamoreno <84933610+anasofiamoreno@users.noreply.github.com>
2021-07-08 16:34:12 -0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,18 +3,26 @@ import data from './data/pokemon/pokemon.js';
 
 console.log(example, data);
 
-let objFiltoUno = document.getElementById("filtrouno");
-let objTexBox1 = document.getElementById("txbxnumeropokemon");
-document.getElementById("btnbuscar").addEventListener("click",filtrar);
-document.getElementById("filtrouno").addEventListener("toggle",fnOcultarTxBx)
+let objFiltoUno = document.getElementById("filtrouno"); // convertir a objeto el Menu desplegable 1
+// elementos del filto
+// Por Numero de pokemon
+// Por Tipo de Pokemon
+// Por Orden Alfabetico
+let objTexBox1 = document.getElementById("txbxnumeropokemon"); //Convertir a objeto el textbox de numero de pokemon
+document.getElementById("btnbuscar").addEventListener("click",filtrar); //listener de boton filtrar o buscar
+document.getElementById("filtrouno").addEventListener("toggle",fnOcultarTxBx) //listener de cambios en menu desplegable 1
 
 //document.getElementById("test").innerHTML = data.pokemon[0]['size']['height'];
 
 
-function filtrar(){
+function fnfiltradoTipo(){
   console.log(objTexBox1.value);
   document.getElementById("test").innerHTML = data['pokemon'][objTexBox1.value-1]['num'];
   document.getElementById("test").innerHTML = data['pokemon'][objTexBox1.value-1]['name'];
+}
+
+function fnFiltadoAlfabetico(){
+  
 }
 
 function fnOcultarTxBx(){
