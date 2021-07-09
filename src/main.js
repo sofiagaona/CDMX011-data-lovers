9047176469fa9af72ee78817a2fcd9734389bbbd
anasofiamoreno <84933610+anasofiamoreno@users.noreply.github.com>
2021-07-09 15:58:41 -0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,14 +1,43 @@
-import { fnFiltradoTipo } from './data.js';
+
 import data from './data/pokemon/pokemon.js';
 import data2 from './data/pokemon/types.js';
 
 
 
+<<<<<<< HEAD
+=======
+import { sortData } from './data.js';
+import {filterData} from './data.js';
+
+var sortby="name";
+var soryOrder="asc";
 
 
-let objFiltoUno = document.getElementById("filtrouno"); // convertir a objeto el Menu desplegable 1
-let objFiltoDos = document.getElementById("filtrodos"); // convertir a objeto el Menu desplegable 2
+var propertyFiltro1= Object.keys(data.pokemon[0]).sort()
+console.log(propertyFiltro1)
+console.log(addOptions(propertyFiltro1))
+//console.log (sortData (data,sortby,soryOrder));
 
+function addOptions(prop) {
+  const objFiltoUno = document.getElementById("filtrouno");
+  
+  for (let value in prop) {
+   var option = document.createElement("option");
+   option.text = prop[value];
+   objFiltoUno.add(option);
+  }
+  }
+
+
+
+
+ 
+>>>>>>> 0776e34da334c3d5b7c8784164c48f9b560b79c8
+
+
+
+/*var filterAlphabe=filterAlphabe(data)
+console.log(filterAlphabe)*/
 // elementos del filto
 // Por Numero de pokemon
 // Por Tipo de Pokemon
@@ -23,6 +52,18 @@ function fnFiltadoAlfabetico(){
   
 }
 
+/*function fnfiltradoTipo(){
+  console.log(objTexBox1.value);
+  document.getElementById("test").innerHTML = data['pokemon'][objTexBox1.value-1]['num'];
+  document.getElementById("test").innerHTML = data['pokemon'][objTexBox1.value-1]['name'];
+}*/
+
+
+/*function fnFiltadoAlfabetico(data){
+  console.log(data)
+  return data
+  
+}*/
 function fnMenuDos(){
   console.log("cambio de menu");
   if(objFiltoDos.option.value =="water"){
@@ -34,13 +75,13 @@ function filtrar(){
   fnFiltradoTipo(data2);
 }
 
-function fnOcultarTxBx(){
+/*function fnOcultarTxBx(){
 
   console.log(objFiltoUno.value);
   if(objFiltoUno.value==1){
 
   }
-}
+}*/
 
 
  
